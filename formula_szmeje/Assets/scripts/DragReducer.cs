cd1b3933ad42fd54df27d2beb46d524e14276b10
Cukierrrrr <rafalfilipczak34@gmail.com>
2026-04-15 21:11:16 +0200

    poprawki błędów
    poprawki błędów

---
+++ formula_szmeje/Assets/scripts/DragReducer.cs
@@ -0,0 +1,56 @@
+using JetBrains.Rider.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragReducer : MonoBehaviour
+{
+    private int detectedCars = 0;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = gameObject.transform.parent.GetComponent<Rigidbody>();
+
+        Collider collider = GetComponent<Collider>();
+
+        Collider[] hits = Physics.OverlapBox(collider.bounds.center, collider.bounds.extents);
+
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Car"))
+            {
+                detectedCars++;
+            }
+        }
+        if (detectedCars > 0)
+        {
+            rb.drag = 0.09f;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            detectedCars++;
+            if(detectedCars == 1)
+            {
+                rb.drag = 0.09f;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Car"))
+        {
+            detectedCars--;
+            if (detectedCars < 0)
+            {
+                detectedCars = 0;
+            }
+            if (detectedCars == 0)
+            {
+                rb.drag = 0.09f;
+            }
+        }
+    }
+}
